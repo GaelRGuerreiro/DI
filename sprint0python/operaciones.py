87e9ccf60a3fc6484b5ea6d115e722b64a0e039e
GaelRGuerreiro <145338668+GaelRGuerreiro@users.noreply.github.com>
2023-10-01 13:59:40 +0200

    Ejercicio 3 python

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,15 @@
+def suma(num1, num2):
+    return num1+num2
+
+def resta(num1, num2):
+    
+        return num1-num2
+
+def multiplicacion(numero1, numero2):
+    return numero1*numero2
+
+def division(numero1,numero2):
+    if  numero2==0:
+        return print("\n No se puede dividir entre 0.")
+    else:
+        return numero1/numero2
