f0fcf08c11b8fd6eed3505a8e8ca6cbf01362b68
GaelRGuerreiro <145338668+GaelRGuerreiro@users.noreply.github.com>
2023-10-27 16:48:54 +0200

    Ejercicio 7 completado

--- sprint2catalog/window.py
+++ sprint2catalog/window.py
@@ -1,55 +1,66 @@
-#window.py
 import requests
 from io import BytesIO
 import tkinter as tk
-from PIL import Image, ImageTk 
+from PIL import Image, ImageTk
 from cell import Cell
 from detail_window import DetailWindow
-from tkinter import Label
+from tkinter import messagebox, Canvas, Scrollbar, Frame, Label
 
-
-    
-#Funcion que devuelve la imagen de cada item del json    
-def load_image_from_url(url):
+# Función que devuelve la imagen de cada item del JSON
+def load_image_from_url(url, width, height):
     response = requests.get(url)
     image_data = Image.open(BytesIO(response.content))
-    image = ImageTk.PhotoImage(image_data)
-    return image
+    image = image_data.resize((width, height), Image.LANCZOS)
+    return ImageTk.PhotoImage(image)
 
+def acerca_de():
+    info = "Este desarrollador es un profesional."
+    messagebox.showinfo("Acerca del desarrollador:", info)
 
 class MainWindow():
-
     def on_button_clicked(self, name, descripcion, image):
         detail_window = DetailWindow(self.root, name, descripcion, image)
 
     def __init__(self, root, json_data):
-        
-        #Variables
         self.root = root
         root.title("MainWindow")
         self.cells = []
 
-        #Centrar la ventana
         x = (self.root.winfo_screenwidth() - self.root.winfo_reqwidth()) / 2
         y = (self.root.winfo_screenheight() - self.root.winfo_reqheight()) / 2
         self.root.geometry(f"+{int(x)}+{int(y)}")
 
+        # Crear el Canvas y el Scrollbar
+        self.canvas = Canvas(root)
+        self.scrollbar = Scrollbar(root, orient="vertical", command=self.canvas.yview)
+
+        # Crear el Frame que será desplazable
+        self.scrollable_frame = Frame(self.canvas)
+        self.scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        # Agregar Canvas y Scrollbar a la ventana
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.scrollbar.grid(row=0, column=1, sticky="ns")
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+
         labels = []
-        #Bucle para recorrer todos los items del JSON descargado y añadirlo a la celda
+
+        # Bucle para recorrer todos los items del JSON descargado y añadirlos al Frame
         for item in json_data:
-            
-            #Guardamos en variables cada elemento del item del JSON
             name = item["name"]
             descripcion = item["descripcion"]
             image_url = item["image_url"]
-            image = load_image_from_url(image_url)
+            image = load_image_from_url(image_url, 200, 200)
 
-            #creamos una nueva variable celda con los datos de la variable de arriba y la añadimos al array donde estarán todas las celdas
             cell = Cell(name, descripcion, image_url, image)
             self.cells.append(cell)
 
-
-            label = Label(root, image=cell.image, text=name, compound=tk.BOTTOM) 
-            label.grid(row=0, column=len(labels))
+            # Creación de etiquetas en el Frame
+            label = Label(self.scrollable_frame, image=cell.image, text=name, compound=tk.BOTTOM)
+            label.grid(row=len(labels), column=0)
             label.bind("<Button-1>", lambda event, cell=cell, name=name, descripcion=descripcion, image=image: self.on_button_clicked(name, descripcion, image))
-            labels.append(label)  
+            labels.append(label)
