b5a4adb1581be9a39e5af020c639bb4eca23a1a5
GaelRGuerreiro <145338668+GaelRGuerreiro@users.noreply.github.com>
2023-11-06 09:25:00 +0100

    Ejercicio 2 Añadida y modificada DetailActivity

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,6 +1,27 @@
 package com.example.mycatalog;
 
-public class CatalogActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+public class CatalogActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        Button buttonNavigateToDetail = findViewById(R.id.buttonDetail); // Utiliza el ID correcto del botón
+
+        buttonNavigateToDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad DetailActivity al hacer clic en el botón
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
